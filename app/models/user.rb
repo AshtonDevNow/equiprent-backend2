e5b2d15b92c367affa97eaecf9050536d71f8849
rami megarbane <rmegarbane@gmail.com>
2021-12-12 15:19:03 +0800

    add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :rentals
+  has_many :products 
+
+  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def print_name
-    puts name
-  end
 end
