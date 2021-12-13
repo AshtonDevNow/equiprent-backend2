3c5bfe7e9860b749b0d0ede94f3294a4bcfd4b87
Ashton Bunn <ashtonwork@hotmail.com>
2021-12-13 20:40:16 +0800

    added table to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :rentals
-  has_many :products 
+  has_many :products
 
-  
-  
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+table,
+    acts_as_token_authenticable # add this status
+    [...]
 
 end
