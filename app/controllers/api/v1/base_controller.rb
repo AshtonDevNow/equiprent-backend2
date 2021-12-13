3a18f626f1701efe1b22eafdbcfba570452f0f76
Ashton Bunn <ashtonwork@hotmail.com>
2021-12-13 21:59:04 +0800

    added token to base controller warning this is before seeding

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,3 @@
 class Api::V1::BaseController < ApplicationController
+  # acts_as_token_authentication_handler_for User, except: [:login]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :rentals
@@ -8,8 +9,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-table,
-    acts_as_token_authenticable # add this status
-    [...]
+
+
 
 end
